03ab266573063f31a8e65c0257fbdae3f1bc23b1
Quinn Zapata <qzapata@uw.edu>
2017-02-28 23:43:30 -0800

    update starter ui.R code

--- ui.R
+++ ui.R
@@ -3,12 +3,20 @@ library('shiny')
 
 # main ui
 shinyUI(fluidPage(
+  # creates title and page title
+  titlePanel(strong('Team Kiwi'), windowTitle='Team Kiwi'),
+  
+  # creates multi-column layout
   sidebarLayout(
     sidebarPanel(
       
     ),
+    
+    # creates main panel for data
     mainPanel(
-      
+      tabsetPanel(
+        
+      )
     )
   )
 ))
