9ed818bd9c7bad82b35277925ec4527814546b3d
Quinn Zapata <qzapata@uw.edu>
2017-03-02 13:31:16 -0800

    complete basic map and features

--- server.R
+++ server.R
@@ -7,6 +7,39 @@ library('maps')
 # store data set
 persons.data <- read.csv('data/unhcr_popstats_export_time_series_all_data.csv', stringsAsFactors = FALSE)
 
+# removes 2014 and convert value col to numeric
+persons.data <- filter(persons.data, Year != '2014')
+persons.data$Value <- sapply(persons.data$Value, as.numeric)
+
 shinyServer(function(input, output) {
+  # filters data for map to use
+  map.filter <- reactive({
+    data <- persons.data %>% 
+      filter(Year == input$year.choice) %>% 
+      filter(Population.type %in% input$type.of.displacement) %>% 
+      mutate(ISO3 = iso.alpha(.[[input$purpose.choice]], n=3))
+    View(data)
+    return(data)
+  })
   
+  # generates map based on user input
+  output$map.plot <- renderPlot({
+    world.map <- map_data('world') %>% 
+      mutate(ISO3 = iso.alpha(.$region, n = 3)) %>% 
+      left_join(map.filter())
+    
+    # create map
+    map <- ggplot(data=world.map) +
+      geom_polygon(mapping=aes(x=long, y=lat, group=group, fill=cut(Value, 9)),
+                   color='black',
+                   size=0.25) +
+      coord_quickmap() +
+      labs(title=paste('Displacement of Persons in', input$year.choice), 
+           fill='Displaced Population', 
+           y='Latitude',
+           x='Longitude') +
+      scale_fill_brewer(type = 'seq', palette = 2, direction = 1)
+    
+    return(map)
+  })
 })
--- ui.R
+++ ui.R
@@ -1,6 +1,14 @@
 # import libraries
 library('shiny')
 
+# imports data set
+persons.data <- read.csv('data/unhcr_popstats_export_time_series_all_data.csv')
+
+# varaibles to speed up load time
+years <- 1951:2013
+pop.types <- levels(persons.data$Population.type)
+country.names <- levels(persons.data$Country...territory.of.asylum.residence)
+
 # main ui
 shinyUI(fluidPage(
   # creates title and page title
@@ -9,13 +17,22 @@ shinyUI(fluidPage(
   # creates multi-column layout
   sidebarLayout(
     sidebarPanel(
-      
+      selectInput('year.choice', label='Year', choice=rev(years)),
+      selectInput('purpose.choice', label='Relation', choice=list('Taking in'='Country...territory.of.asylum.residence',
+                                                                  'Fleeing'='Origin')),
+      checkboxGroupInput('type.of.displacement', label='Type of Displacement', choice=pop.types, 
+                         selected=pop.types),
+      selectInput('map.type', label='Map Type', choice=list('Color'='color.map.plot', 
+                                                            'Line'='line.map.plot')),       
+      selectInput('country.choice', label='Country', choice=c('All', country.names))
     ),
     
     # creates main panel for data
     mainPanel(
       tabsetPanel(type='tabs',
-                  
+            tabPanel('Map',
+                     plotOutput('map.plot'))
+                     #textOutput('map.text'))      
       )
     )
   )
