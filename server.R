e5ecbea2366afb6a26f0bc46f74fcd1cb8605fe0
victoriabaik <eunseo@uw.edu>
2017-03-06 21:23:45 -0800

    Added Graph

--- server.R
+++ server.R
@@ -3,10 +3,46 @@ library('shiny')
 library('dplyr')
 library('ggplot2')
 library('maps')
+library('plotly')
+library('dygraphs')
 
 # store data set
 persons.data <- read.csv('data/unhcr_popstats_export_time_series_all_data.csv', stringsAsFactors = FALSE)
 
+# removes 2014 and convert value col to numeric
+persons.data <- filter(persons.data, Year != '2014')
+persons.data$Value <- sapply(persons.data$Value, as.numeric)
+
+
 shinyServer(function(input, output) {
   
+  # create filtered data for line graph
+  bar.filter <- reactive({
+    data <- persons.data %>% 
+      filter(Population.type %in% input$type.of.displacement) %>% 
+      mutate(ISO3.residence = iso.alpha(.$Country...territory.of.asylum.residence, n=3),
+             ISO3.origin = iso.alpha(.$Origin, n=3)) 
+    
+      if (input$direction.choice == 'ISO3.origin'){
+        data <- filter(data, Country...territory.of.asylum.residence == input$country.choice)
+        data <- select(data, Year, Value)
+        } else {
+        data <- filter(data, Origin == input$country.choice)
+        data <- select(data, Year, Value)
+      }
+
+    return(data)
+  })
+  
+  # Generate Dygraph for selected country
+  output$dygraph <- renderDygraph({
+        dygraph(bar.filter(), main = paste("Displacement Graph for", input$country.choice)) %>%
+          dySeries("Value", label = "Population") %>%
+          dyRangeSelector() %>%
+          dyOptions(stepPlot = TRUE)
+  })
+    
 })
+    
+  
+
--- ui.R
+++ ui.R
@@ -1,5 +1,13 @@
 # import libraries
 library('shiny')
+library('plotly')
+
+# import data
+persons.data <- read.csv('data/unhcr_popstats_export_time_series_all_data.csv')
+
+# Variables for tab
+pop.types <- levels(persons.data$Population.type)
+country.names <- levels(persons.data$Country...territory.of.asylum.residence)
 
 # main ui
 shinyUI(fluidPage(
@@ -9,14 +17,23 @@ shinyUI(fluidPage(
   # creates multi-column layout
   sidebarLayout(
     sidebarPanel(
-      
+      # universal panels
+ #     selectInput('year.choice', label='Year', choice=rev(years)),
+      checkboxGroupInput('type.of.displacement', label='Type of Displacement', choice=pop.types,
+                         selected=pop.types[1]),
+      selectInput('direction.choice', label='Purpose', 
+                  choice=list('Fleeing'='ISO3.residence', 'Accepting'='ISO3.origin')),
+      selectInput('country.choice', label='Country', 
+                  choice=c('All', country.names), selected=country.names[1]),
+      hr(),
+      helpText("Data from UNHCR")
     ),
     
     # creates main panel for data
     mainPanel(
       tabsetPanel(type='tabs',
-                  
-      )
+                  tabPanel('Line Graph', hr(), dygraphOutput("dygraph"))
+    )    
     )
-  )
-))
+ 
+  )))
