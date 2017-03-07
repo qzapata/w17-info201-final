b6ff725733eb72f33448618f3415e25e19cd1a53
Clayton Vo <voclayton@gmail.com>
2017-03-06 19:32:54 -0800

    Complete table

--- server.R
+++ server.R
@@ -7,6 +7,51 @@ library('maps')
 # store data set
 persons.data <- read.csv('data/unhcr_popstats_export_time_series_all_data.csv', stringsAsFactors = FALSE)
 
+# removes 2014 and convert value col to numeric
+persons.data <- filter(persons.data, Year != '2014')
+persons.data$Value <- sapply(persons.data$Value, as.numeric)
+
 shinyServer(function(input, output) {
+  
+  # Creates filtered data frame as reactive variable
+  filtered.data <- reactive ({
+    persons.filtered <- 
+      if (input$table.type == "Residence") {
+        filter(persons.data, Country...territory.of.asylum.residence == input$country.choice)
+      } else {
+        filter(persons.data, Origin == input$country.choice) 
+      }
+    if (input$year.choice != "All") {
+      persons.filtered <- filter(persons.filtered, Year == input$year.choice)
+    }
+    persons.filtered <- filter(persons.filtered, Population.type %in% input$type.of.displacement) %>%
+                        arrange(desc(Value))
+    if(nrow(persons.filtered) > input$row.num) {
+      persons.filtered <- persons.filtered[0:input$row.num,]
+    }
+    return(persons.filtered)
+  })
+  
+  # Creates table of the top countries where people are either going to or coming from.
+  output$table <- renderTable({
+    if(nrow(filtered.data()) == 0) {
+      return()
+    } else {
+      table.data <- filtered.data()
+      table.data$Value <- format(table.data$Value,big.mark=",",scientific=FALSE)
+      colnames(table.data) <- c("Year", "Country of Residence", "Origin","Type of Displacement", "Number of People")
+    return(table.data)
+    }
+  })
+  
+  # Message letitng the user know that there are no values found.
+  output$text <- renderText({
+    if(nrow(filtered.data()) == 0) {
+      return("No data found for the active filters.")
+    }
+  })
+
+    
+
   
 })
--- ui.R
+++ ui.R
@@ -1,6 +1,15 @@
 # import libraries
 library('shiny')
 
+
+# import data set
+persons.data <- read.csv('data/unhcr_popstats_export_time_series_all_data.csv')
+
+# variables to use
+years <- 1951:2013
+pop.types <- levels(persons.data$Population.type)
+country.names <- levels(persons.data$Country...territory.of.asylum.residence)
+
 # main ui
 shinyUI(fluidPage(
   # creates title and page title
@@ -9,12 +18,27 @@ shinyUI(fluidPage(
   # creates multi-column layout
   sidebarLayout(
     sidebarPanel(
+      conditionalPanel(
+        condition='input.tabPanel=="Table"',
+        selectInput('country.choice', label='Country', choice=c(country.names), selected=country.names[1])),
+        selectInput('year.choice', label='Year', choice=c("All",rev(years))),
+        checkboxGroupInput('type.of.displacement', label='Type of Displacement', choice=pop.types, 
+                         selected=pop.types),
+        radioButtons("table.type", "Table Information:", c("Origin","Residence")),
+        numericInput('row.num', label = 'Max Rows', value = 10, min = 0)
       
-    ),
+      )
+    ,
     
     # creates main panel for data
-    mainPanel(
-      tabsetPanel(type='tabs',
+  mainPanel(
+      tabsetPanel(type='tabs', id = 'tabPanel',
+                  tabPanel('Table',
+                      p(" The following table includes information about where displaced persons are going and where they are coming from, arranged descendingly
+                        according to the number of people displaced:"),
+                      textOutput('text'),
+                      tableOutput('table')
+                  )
                   
       )
     )
